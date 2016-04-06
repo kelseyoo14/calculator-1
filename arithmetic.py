d467afe0fff8351305aac79604d026ec5e0b160d
Hackbright Student <info@hackbrightacademy.com>
2016-04-06 12:20:32 -0700

    Initial Commit

---
+++ arithmetic.py
@@ -0,0 +1,30 @@
+def add(num1, num2):
+    """Returns the sum of the 1st number and 2nd number"""
+    return num1 + num2
+
+def subtract(num1, num2):
+    """Returns the difference between 1st  and 2nd number"""
+    return num1 - num2
+
+def multiply(num1, num2):
+    """Returns the product of the 1st and 2nd number"""
+    return num1 * num2
+
+def divide(num1, num2):
+    """Returns the quotient of 1st divided by the 2nd number"""
+    return num1 / num2
+
+def square(num1):
+    """Returns the square of the input"""
+    return num1 ** 2
+
+def cube(num1):
+    """Returns the cube of the input"""
+    return num1 ** 3
+
+def power(num1, num2):
+    """Returns the 1st number raised to the power of the 2nd"""
+    return num1 ** num2
+def mod(num1, num2):
+    """Returns the modulo of the 1st number divided by second number"""
+    return num1 % num2
